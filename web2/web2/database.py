eb32b06a56dcbe7bcec3f68b8e858c2a6ee159e8
RA-DM <61676742+RA-DM@users.noreply.github.com>
2020-03-30 12:15:09 +0800

    Add files via upload

---
+++ web2/web2/database.py
@@ -0,0 +1,16 @@
+from config import db
+import mysql.connector
+
+conn = mysql.connector.connect(host=db['host'], user=db['user'], passwd=db['passwd'], 
+                                database=db['database'], charset='utf8mb4')
+db = conn.cursor()
+
+def createApplicant(name,gender,grade,area,college,phone,department,faceTime,introduction):
+    db.execute('''insert into spring (`name`, `gender`,`grade`,`area`,`colleage`,`phone`,`department`,`faceTime`,`introduction`) VALUES (%s, %s, %s, %s, %s, %s, %s,%s,%s)''', (name,gender,grade,area,college,phone,department,faceTime,introduction))
+    conn.commit()
+    return db.rowcount
+
+def findPhone(phone):
+    db.execute('select * from spring where `phone`=%s',(phone,))
+    result = db.fetchall()
+    return result
